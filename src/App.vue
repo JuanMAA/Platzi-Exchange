31752eb4836091da06f4906b4f69add6c4b7798d
juanmansillaasenjo <juan.mansilla@opciones.cl>
2020-12-24 20:40:32 -0300

    Class 1 to 29

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main>      
+    <px-header/>
+    <px-assets-table/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader"
+import PxAssetsTable from "@/components/PxAssetsTable"
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { PxHeader, PxAssetsTable }
 };
 </script>
 
 <style>
-#app {
+
+    PxAssetsTable#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
